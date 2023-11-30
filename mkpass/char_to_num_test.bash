bcc9b7bfcf9044e412d1228f5aa95278f1e018de
Etsushi Hachiya <tedis.hachiya@gmail.com>
2023-11-30 13:42:29 +0900

    fix

--- mkpass/char_to_num_test.bash
+++ mkpass/char_to_num_test.bash
@@ -16,14 +16,26 @@ out=$(python3 char_to_num a b c d)
 [ "${out}" = "1 2 3 4  " ] || ng ${LINENO}
 
 # STRANGE INPUT
-out=$(python3 char_to_num 1 2 3 4)
+out=$(python3 char_to_num a ~ c ¥) #記号
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "1 " ] || ng ${LINENO} #記号が読み込まれた箇所でキーエラー
+
+out=$(python3 char_to_num 1 2 3 4) #数字
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "1 2 3 4  " ] || ng ${LINENO}
+
+
+out=$(python3 char_to_num abcd) #空白無し
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(python3 char_to_num 1234)
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(python3 char_to_num) #空文字
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 
 [ "$res" = 0 ] && echo OK
