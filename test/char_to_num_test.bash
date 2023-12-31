ebc8a60b582d4e60b30df74e918444d92acc282d
Etsushi Hachiya <tedis.hachiya@gmail.com>
2023-12-31 17:14:55 +0900

    fix

--- test/char_to_num_test.bash
+++ test/char_to_num_test.bash
@@ -12,28 +12,28 @@ res=0
 
 # char_to_num TEST
 # I/O TEST
-out=$(python3 ~/robosys/mkpass/char_to_num a b c d)
+out=$(python3 mkpass/char_to_num a b c d)
 [ "${out}" = "1 2 3 4  " ] || ng ${LINENO}
 
 # STRANGE INPUT
-out=$(python3 ~/robosys/mkpass/char_to_num a ~ c ¥) #記号
+out=$(python3 mkpass/char_to_num a ~ c ¥) #記号
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "1 " ] || ng ${LINENO} #記号が読み込まれた箇所でキーエラー
 
-out=$(python3 ~/robosys/mkpass/char_to_num 1 2 3 4) #数字
+out=$(python3 mkpass/char_to_num 1 2 3 4) #数字
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "1 2 3 4  " ] || ng ${LINENO}
 
 
-out=$(python3 ~/robosys/mkpass/char_to_num abcd) #空白無し
+out=$(python3 mkpass/char_to_num abcd) #空白無し
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(python3 ~/robosys/mkpass/char_to_num 1234)
+out=$(python3 mkpass/char_to_num 1234)
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(python3 ~/robosys/mkpass/char_to_num) #空文字
+out=$(python3 mkpass/char_to_num) #空文字
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
